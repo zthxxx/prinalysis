ee86b8669ccc35f3c3a7958c99e7d58566ed7f1e
zthxxx <zth_9451@qq.com>
2017-08-12 10:55:44 +0800

    Initially complete page home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,75 +2,80 @@
   <div class="content container">
     <div class="index">
       <div class="banner">
-        <img src="../assets/home-banner.jpg" alt="">
+        <img src="../assets/home/home-banner.jpg" alt="">
         <div class="float-div">
           <div class="div-center">
-            <el-button type="primary" class="print" size="large">立即打印</el-button>
+            <el-button type="primary" class="print-btn" size="large">立即打印</el-button>
           </div>
         </div>
+      </div>
+      <depictCard :description="description"></depictCard>
+      <div class="depict-foot">
+        <div>
+          <img src="../assets/home/getup.png" class="foot-img-left">
+          <img src="../assets/home/struggle.png" class="foot-img-right">
+          <div>
+            开学加油哦<br/>
+            云打印解烦恼~<br/>
+            <el-button type="info" class="toPrint">
+              <span>赶紧快印一波 &gt;</span>
+            </el-button>
+          </div>
+        </div>
+      </div>
+      <div class="cooperation">
+        <span class="payMethod">支付方式：</span>
+        <img src="../assets/home/wxPay.png" alt="wxPay">
+        <img src="../assets/home/aliPay.png" alt="aliPay">
+      </div>
+      <div class="foot-section">
+        Copyright © 2017 xxxx All Rights Reserved ICP备100000000-1
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import depictCard from '@/components/DepictCard'
   export default {
     name: 'home',
     data () {
-      return {}
+      return {
+        description: [
+          {
+            img: require('@/assets/home/des-1.png'),
+            title: '快印预先打印，无需等待立即取件',
+            depicts: [
+              'xxxxxxxxxXXXXXXXXxxxxxxxxxxxx',
+              'oooooooooooOOOOOOOOOOOOOooooooooOOOOOOOOOOOOooooooo',
+              'ooooOOOooOOooOOOOOoOoOoOooooOooxooooOoooo'
+            ]
+          },
+          {
+            img: require('@/assets/home/des-1.png'),
+            title: '快印预先打印，无需等待立即取件',
+            depicts: [
+              'xxxxxxxxxXXXXXXXXxxxxxxxxxxxx',
+              'oooooooooooOOOOOOOOOOOOOooooooooOOOOOOOOOOOOooooooo',
+              'ooooOOOooOOooOOOOOoOoOoOooooOooxooooOoooo'
+            ]
+          },
+          {
+            img: require('@/assets/home/des-1.png'),
+            title: '快印预先打印，无需等待立即取件',
+            depicts: [
+              'xxxxxxxxxXXXXXXXXxxxxxxxxxxxx',
+              'oooooooooooOOOOOOOOOOOOOooooooooOOOOOOOOOOOOooooooo',
+              'ooooOOOooOOooOOOOOoOoOoOooooOooxooooOoooo'
+            ]
+          }
+        ]
+      }
     },
-    methods: {},
-    components: {}
+    components: {depictCard}
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  @import "../style/_variables"
-  .content
-    width: 1080px
-    margin: 0 auto
-
-  .index
-    min-width: 1200px
-    width: 100%
-    position: absolute
-    background-color: #fff
-    left: 0
-
-  .banner
-    position: relative
-    text-align: center
-    background-color: color-banner-back
-    overflow: hidden
-    img
-      width: 100%
-      height: auto
-
-  .float-div
-    text-align: left
-    width: 100%
-    position: absolute
-    left: 0
-    top: 0
-    .div-center
-      width: 500px
-      margin: 0 auto
-      position: relative
-      text-align: center
-      padding-top: 300px
-
-  .print
-    padding: 16px 28px
-    font-size: 18px
-    border-radius: 28px
-    border: 1px solid deep-black
-    cursor: pointer
-    background-color: transparent
-    color: deep-black
-    transition: border-color .3s, background-color .3s, color .3s;
-    &:hover
-      color: #fff
-      border: 1px solid color-theme
-      background-color: color-theme
-
+  @import "../style/home"
 </style>
